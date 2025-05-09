e566c6bebf5dd05ed198271d98ab9206d0635bfb
Raaj14Gi <raajboopathy@gmail.com>
2025-05-09 18:10:28 +0530

    create a array with input size and value and find the middle value

--- Hello.java
+++ Hello.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class Hello{
 
   public static void main (String args[]){
@@ -7,7 +8,27 @@ System.out.println(i +" X 2 = " + i*2);
 
 }
 
+Scanner scan = new Scanner(System.in);
 
+System.out.println("enter the size of the array");
+int size = scan.nextInt();
+
+
+int a[] = new int[size];
+
+for ( int x = 0; x <= (size -1); x++ ){
+  System.out.println("enter array value" + "a[" +x + "]");
+  a[x] = scan.nextInt();
+  
+}
+
+for (int x = 0; x < size; x++ ){
+
+System.out.println("a" + "[" + x+"] = " + a[x]);
+}
+int half = ((size/2) );
+
+System.out.println("middle of the array a is " + half + " value is "+ a[half]);
 
   }
 }
