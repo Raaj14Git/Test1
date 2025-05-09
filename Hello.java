40bd6f51caf124fe7c778bcad3b674b4277b3514
Raaj14Gi <raajboopathy@gmail.com>
2025-05-09 14:38:31 +0530

    array

--- Hello.java
+++ Hello.java
@@ -1,14 +1,17 @@
+import java.util.Scanner;
+
 class Hello{
   public static void main(String args[]){
+int[] mark = new int[5];
+ 
+Scanner scan = new Scanner(System.in);
+ mark[0] = scan.nextInt();
+ mark[1] = scan.nextInt();
+ mark [2]= scan.nextInt();
+ mark [3] = scan.nextInt();
+ mark [4]= scan.nextInt();
+ int sum = mark[0] + mark[1] + mark[2] + mark[3]+ mark [4];
 
-    for( int i = 1 ; i<=100 ; i++)
-         {
+ System.out.println(sum);
 
-            if (i%3 ==0 && i%5 ==0){
-//even_count = even_count+1;
-System.out.println(i);
-
-            }
-        }
-        
   }}
