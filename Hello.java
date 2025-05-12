412f7cd9f961c18c644b3bc60aead9c1a27091f9
Raaj14Gi <raajboopathy@gmail.com>
2025-05-12 10:16:50 +0530

    create star right anglr pattern with for loop

--- Hello.java
+++ Hello.java
@@ -1,34 +1,17 @@
-import java.util.Scanner;
+
 class Hello{
 
-  public static void main (String args[]){
-for (int i =1 ; i<=10; i++){
-System.out.println(i +" X 2 = " + i*2);
+  public static void main(String args[]){
+
+    for (int i= 1 ; i<=3; i++){
+
+      System.out.println();
 
 
-}
+      for (int j =1 ; j<=i; j++){
 
-Scanner scan = new Scanner(System.in);
-
-System.out.println("enter the size of the array");
-int size = scan.nextInt();
-
-
-int a[] = new int[size];
-
-for ( int x = 0; x <= (size -1); x++ ){
-  System.out.println("enter array value" + "a[" +x + "]");
-  a[x] = scan.nextInt();
-  
-}
-
-for (int x = 0; x < size; x++ ){
-
-System.out.println("a" + "[" + x+"] = " + a[x]);
-}
-int half = ((size/2) );
-
-System.out.println("middle of the array a is " + half + " value is "+ a[half]);
-
+        System.out.print("*");
+      }
+    }
   }
 }
