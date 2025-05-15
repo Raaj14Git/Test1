347b0c17000b40db94e1581f2f461e882af0745b
Raaj14Gi <raajboopathy@gmail.com>
2025-05-15 13:16:14 +0530

    return keyword

--- Hello.java
+++ Hello.java
@@ -1,17 +1,15 @@
+public class Hello{
 
-class Hello{
+int sum (int a , int b)
+{
+  //int sum11  = a+b;
+return ( a+b);
+}
 
-  public static void main(String args[]){
+  public static void main (String args[]){
 
-    for (int i= 1 ; i<=3; i++){
-
-      System.out.println();
-
-
-      for (int j =1 ; j<=i; j++){
-
-        System.out.print("*");
-      }
-    }
+Hello obj1 = new Hello();
+int sum1 = obj1.sum(30,6);
+System.out.println(sum1);
   }
 }
